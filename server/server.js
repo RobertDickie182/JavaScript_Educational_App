27eb369819240c4cb8e2497901387f775c1f3466
RobertDickie182 <robertdanieldickie@gmail.com>
2023-07-28 15:20:28 +0100

    filling seeds file

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+    .then((client) => {
+        const db = client.db('scienceHub'); 
+        const articlesCollection = db.collection('articles'); 
+        const articlesRouter = createRouter(articlesCollection); 
+        app.use('/api/articles', articlesRouter); 
+    })
+    .catch(console.error);
+
+
+
+app.listen(9000, function () {
+    console.log(`Listening on port ${this.address().port}`);
+});
