38ff23f5fc7bc2db3332a13aeb634d791e05f8be
AlexandraViasu <viasu.alexandra@gmail.com>
2023-07-31 13:41:33 +0100

    fixed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import ScienceHubContainer from './Containers/ScienceHubContainer';
 import ErrorPage from "./Components/ErrorPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import TodaysQuiz from "./Components/TodaysQuiz";
+import TopScores from "./Components/TopScores";
 
 
 function App() {
@@ -11,8 +12,8 @@ function App() {
   <Router>     
       <Routes>
         <Route path="/articles" element={<ScienceHubContainer heading="Science Hub"/>}/>
-        {/* <Route path="/todaysquiz" element={<TodaysQuiz heading="Today's Quiz"/>}/> 
-        <Route path="/topscores" element={<TopScores heading="Top Scores"/>}/> */}
+        <Route path="/todaysquiz" element={<TodaysQuiz heading="Today's Quiz"/>}/> 
+        <Route path="/topscores" element={<TopScores heading="Top Scores"/>}/>
         <Route path="/*" element={<ErrorPage/>}/>
       </Routes>
     
