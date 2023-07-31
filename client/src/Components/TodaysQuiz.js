42c6fabe5c404e7e016e37f2b17017e32eaf00f1
AlexandraViasu <viasu.alexandra@gmail.com>
2023-07-31 17:31:26 +0100

    removed extra links from other pages

--- client/src/Components/TodaysQuiz.js
+++ client/src/Components/TodaysQuiz.js
@@ -18,11 +18,7 @@ const TodaysQuiz = () => {
     
     return(
         <>
-        <header className='header'>
-            <Link to="/articles">Science Hub | </Link>
-            <Link to="/todaysquiz">Today's Quiz | </Link>
-            <Link to="/topscores">  Top Scores</Link>
-        </header>
+       
         </>
     )
 }
